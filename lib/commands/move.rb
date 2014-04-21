658f0816523ef7c5f8aac1f4e99aa242d9e57ed1
Norbert_Csibra <Norbert_Csibra@epam.com>
2014-04-21 10:01:31 +0200

    Change move to easily switch between two workspaces.

--- lib/commands/move.rb
+++ lib/commands/move.rb
@@ -5,7 +5,7 @@ module PerforceTools
     def self.run(arguments)
       perforce = PerforceTools.connection
       changelist = arguments[:changelist]
-      is_not_empty = empty_changelist?(changelist)
+      is_not_empty = !empty_changelist?(changelist)
 
       if arguments[:shelve] && is_not_empty
         perforce.run_shelve('-f', '-c', changelist)
@@ -15,6 +15,7 @@ module PerforceTools
         perforce.run_revert('-w', '-c', changelist, '//...')
       end
 
+      changelist_spec = perforce.fetch_change(changelist)
       if arguments[:switch]
         workspaces = arguments[:switch]
 
@@ -22,7 +23,7 @@ module PerforceTools
           raise(ArgumentError, 'The switch parameter need to contains 2 workspace names exactly!')
         end
 
-        current = p4.client
+        current =  changelist_spec['Client']
         unless workspaces.delete(current)
           raise(ArgumentError, "The switch parameter does not contains the currently active workspace: #{current}!")
         end
@@ -32,15 +33,14 @@ module PerforceTools
         new_workspace = arguments[:workspace]
       end
 
-      change_spec = perforce.fetch_change(changelist)
-      change_spec['Client'] = new_workspace
-      perforce.save_change(change_spec)
+      changelist_spec['Client'] = new_workspace
+      perforce.save_change(changelist_spec)
     end
 
     def self.set_options(opts)
       opts.set do
         arg :workspace, 'Name of the new workspace.', :short => '-w', :type => :string
-        arg :switch, 'Switch between workspaces, only 2 workspace name allowed.', :short => '-i', :type => :strings, :multi => true
+        arg :switch, 'Switch between workspaces, only 2 workspace name allowed.', :short => '-i', :type => :strings
         arg :changelist, 'Changelist number.', :short => '-c', :type => :int, :required => true
         arg :revert, 'Revert before move.', :short => '-r'
         arg :shelve, 'Shelve before move.', :short => '-s'
