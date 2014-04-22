4fd0d4ccfe47eb5097e100d0f5fffb31d2261a3f
Norbert_Csibra <Norbert_Csibra@epam.com>
2014-04-22 14:35:36 +0200

    Refactoring shelve

--- lib/commands/shelve.rb
+++ lib/commands/shelve.rb
@@ -1,8 +1,9 @@
 module P4Tools
-  module Shelve
+  class Shelve
+    include CommandUtils
 
     def self.run(arguments)
-      Shelver.new(arguments).shelve
+      Shelve.new(arguments).run
     end
 
     def self.set_options(opts)
@@ -12,40 +13,37 @@ module P4Tools
       end
     end
 
-    class Shelver
-      include CommandUtils
 
-      def initialize(args)
-        @files = args[:files]
-        @shelve_changelist = args[:changelist] || create_new_changelist("Shelve container for files:\n\n#{@files.join("\n")}")
-        @p4 = P4Tools.connection
-      end
+    def initialize(args)
+      @files = args[:files]
+      @shelve_changelist = args[:changelist] || create_new_changelist("Shelve container for files:\n\n#{@files.join("\n")}").to_i
+      @p4 = P4Tools.connection
+    end
 
-      def shelve
-        current_changelist = get_current_changelist
+    def run
+      current_changelist = get_current_changelist
 
-        move_to(@shelve_changelist)
-        shelve_all
-        move_to(current_changelist)
-      end
+      move_to(@shelve_changelist)
+      shelve
+      move_to(current_changelist)
+    end
 
 
-      private
+    private
 
-      def get_current_changelist
-        file_info = @p4.run_opened('-u', @p4.user, @files.first).first
-        file_info['change']
-      end
+    def get_current_changelist
+      file_info = @p4.run_opened('-u', @p4.user, @files.first).first
+      file_info['change']
+    end
 
-      def shelve_all
-        @p4.run_shelve('-f', '-c', @shelve_changelist, @files)
-      end
+    def shelve
+      @p4.run_shelve('-f', '-c', @shelve_changelist, @files)
+    end
 
-      def move_to(changelist)
-        @p4.run_reopen('-c', changelist, @files)
-      end
-
+    def move_to(changelist)
+      @p4.run_reopen('-c', changelist, @files)
     end
 
   end
+
 end
